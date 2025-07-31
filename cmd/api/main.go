71af4e676868235e0879991c87761c8fb9b54b59
“registaalfe” <alfexcrstds07@gmail.com>
2025-07-31 23:58:33 +0700

    feat: understanded setup main entry point for GO API service

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,21 +2,21 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"net/http" // provide HTTP client and server implementations
 
-	"github.com/go-chi/chi"
-	"github.com/registaalfe/authuser-api-golang.git/internal/handlers"
-	log "github.com/sirupsen/logrus"
+	"github.com/go-chi/chi"                                            // router for HTTP request routing like /users/{id}
+	"github.com/registaalfe/authuser-api-golang.git/internal/handlers" // saving the logic every endpoint like /coin-balance
+	log "github.com/sirupsen/logrus"                                   // library logging in Go to show log error/info with neat format
 )
 
 func main() {
-	log.SetReportCaller(true)
+	log.SetReportCaller(true) // Set the log to show the file and line number where the log was called
 
-	var r *chi.Mux = chi.NewRouter()
-	handlers.Handler(r)
+	var r *chi.Mux = chi.NewRouter() // Create a new router instance using chi
+	handlers.Handler(r)              // Call the Handler function to register all routes and their handlers
 
 	fmt.Println("Starting GO API service...")
-	err := http.ListenAndServe("localhost:8000", r)
+	err := http.ListenAndServe("localhost:8000", r) // Start the HTTP server on localhost:8000 with the router r
 	if err != nil {
 		log.Error(err)
 	}
