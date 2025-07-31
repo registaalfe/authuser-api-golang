56afed15b4c317f60d2dc750a72b7f7bedf8320e
“registaalfe” <alfexcrstds07@gmail.com>
2025-08-01 03:04:10 +0700

    feat: added documentation for fetching user coin details

--- internal/handlers/get_coin_balance.go
+++ internal/handlers/get_coin_balance.go
@@ -1,37 +1,40 @@
-package handlers
+package handlers // Signify the package name is on handlers package
 
 import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/gorilla/schema"
-	"github.com/registaalfe/authuser-api-golang.git/api"
-	"github.com/registaalfe/authuser-api-golang.git/internal/tools"
+	"github.com/gorilla/schema"                                     // Package for change query parameters to struct
+	"github.com/registaalfe/authuser-api-golang.git/api"            // Package for API definitions
+	"github.com/registaalfe/authuser-api-golang.git/internal/tools" // Package for connecting to the database
 	log "github.com/sirupsen/logrus"
 )
 
 func GetCoinBalance(w http.ResponseWriter, r *http.Request) {
-	var params = api.CoinBalanceParams{}
-	var decoder *schema.Decoder = schema.NewDecoder()
-	var err error
+	var params = api.CoinBalanceParams{}              // Take the data from CoinBalanceParams struct
+	var decoder *schema.Decoder = schema.NewDecoder() // To convert query parameters to struct
+	var err error                                     // To handle errors
 
-	err = decoder.Decode(&params, r.URL.Query())
+	err = decoder.Decode(&params, r.URL.Query()) // Save the query parameters to params struct
 
+	// Check if the username is empty
 	if err != nil {
 		log.Error(err)
 		api.InternalErrorHandler(w)
 		return
 	}
 
-	var database *tools.DatabaseInterface
-	database, err = tools.NewDatabase()
+	// Get the databaseInterface and save it to database variable
+	var database *tools.DatabaseInterface // Declare a variable to hold the database interface with pointer type
+	database, err = tools.NewDatabase()   // Call func NewDatabase to get the database interface
 	if err != nil {
 		api.InternalErrorHandler(w)
 		return
 	}
 
-	var tokenDetails *tools.CoinDetails
-	tokenDetails = (*database).GetUserCoins(params.Username)
+	// Get the data coins by get the username from the database
+	var tokenDetails *tools.CoinDetails                      // Declare a variable as a pointer to CoinDetails struct
+	tokenDetails = (*database).GetUserCoins(params.Username) // Get data from database
 	if tokenDetails == nil {
 		log.Error(err)
 		api.InternalErrorHandler(w)
@@ -43,8 +46,8 @@ func GetCoinBalance(w http.ResponseWriter, r *http.Request) {
 		Code:    http.StatusOK,
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(response)
+	w.Header().Set("Content-Type", "application/json") // Set the data type in the response header to JSON
+	err = json.NewEncoder(w).Encode(response)          // Changes struct Go to JSON format
 	if err != nil {
 		log.Error(err)
 		api.InternalErrorHandler(w)
